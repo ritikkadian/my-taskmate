e4e79a087ab4bb1b9d473dffd525031cdd47b428
Ritik <ritik1kadian@gmail.com>
2023-08-29 22:33:04 +0530

    Added JSX

--- src/App.js
+++ src/App.js
@@ -1,3 +1,30 @@
+import "./styles.css";
 export default function App() {
-  return "Hello";
+  return (
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">NewItem</label>
+          <input type="text" id="item" />
+        </div>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 2
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  );
 }
